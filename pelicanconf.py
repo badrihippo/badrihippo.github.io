b7f54f23bee54e2d2a8b7a2afa4391a476afb961
Hippo <badrihippo@gmail.com>
2015-07-09 19:19:45 +0530

    Changed theme to "Alchemy"

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Hippo'
-SITENAME = u'~badrihippo'
+SITENAME = u'badrihippo'
 SITEURL = ''
 
 PATH = 'content'
@@ -32,8 +32,19 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = 'themes/pelican-bootstrap3'
+THEME = 'themes/pelican-alchemy/alchemy'
 
 OUTPUT_PATH = '../master/'
 
 STATIC_PATHS = ['images']
+
+# Theme-specific settings (for pelican-alchemy)
+
+EXTRA_FAVICON = True
+PAGES_ON_MENU = True
+CATEGORIES_ON_MENU = True
+SHOW_ARTICLE_AUTHOR = False
+GITHUB_ADDRESS = 'https://github.com/badrihippo'
+PROFILE_IMAGE = '//i.imgur.com/NOZOY.gif'
+SITE_SUBTEXT = 'programming and more'
+SO_ADDRESS = 'http://stackoverflow.com/users/1196444/hippo'
